d289a3633c4f88e787efc81bd2fd57688975ca3a
9qg0ph <9qg0ph@sina.com>
2026-01-26 01:26:58 +0800

    创建 Skullgirls dylib - 实现互秒功能

---
+++ SkullgirlsDylib/SkullgirlsCheat.m
@@ -0,0 +1,254 @@
+// 骷髅少女修改器 - SkullgirlsCheat.m
+#import <UIKit/UIKit.h>
+
+#pragma mark - GameForFun 引擎接口
+
+@interface FanhanGGEngine : NSObject
++ (instancetype)sharedInstance;
+- (void)setValue:(id)value forKey:(NSString *)key withType:(NSString *)type;
+@end
+
+// 辅助函数：调用 GameForFun 设置参数
+static void setGameValue(NSString *key, id value, NSString *type) {
+    FanhanGGEngine *engine = [FanhanGGEngine sharedInstance];
+    if (engine) {
+        [engine setValue:value forKey:key withType:type];
+    }
+}
+
+#pragma mark - 全局变量
+
+@class SGMenuView;
+static UIButton *g_floatButton = nil;
+static SGMenuView *g_menuView = nil;
+
+#pragma mark - 菜单视图
+
+@interface SGMenuView : UIView
+@property (nonatomic, strong) UIView *contentView;
+@end
+
+@implementation SGMenuView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) { [self setupUI]; }
+    return self;
+}
+
+- (void)setupUI {
+    self.backgroundColor = [UIColor colorWithWhite:0 alpha:0.5];
+    
+    CGFloat contentHeight = 280;
+    CGFloat contentWidth = 280;
+    CGFloat viewWidth = self.bounds.size.width;
+    CGFloat viewHeight = self.bounds.size.height;
+    
+    self.contentView = [[UIView alloc] initWithFrame:CGRectMake(
+        (viewWidth - contentWidth) / 2,
+        (viewHeight - contentHeight) / 2,
+        contentWidth, contentHeight
+    )];
+    self.contentView.backgroundColor = [UIColor colorWithWhite:1 alpha:0.95];
+    self.contentView.layer.cornerRadius = 16;
+    self.contentView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
+    [self addSubview:self.contentView];
+    
+    // 关闭按钮
+    UIButton *closeButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    closeButton.frame = CGRectMake(contentWidth - 40, 0, 40, 40);
+    closeButton.backgroundColor = [UIColor colorWithWhite:0.9 alpha:1];
+    closeButton.layer.cornerRadius = 20;
+    [closeButton setTitle:@"✕" forState:UIControlStateNormal];
+    [closeButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
+    closeButton.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+    [closeButton addTarget:self action:@selector(closeMenu) forControlEvents:UIControlEventTouchUpInside];
+    [self.contentView addSubview:closeButton];
+    
+    // 标题
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(20, 5, contentWidth - 60, 30)];
+    title.text = @"💀 骷髅少女";
+    title.font = [UIFont boldSystemFontOfSize:20];
+    title.textColor = [UIColor colorWithRed:0.8 green:0.2 blue:0.4 alpha:1];
+    title.textAlignment = NSTextAlignmentCenter;
+    [self.contentView addSubview:title];
+    
+    CGFloat y = 45;
+    
+    // 学习提示
+    UILabel *info = [[UILabel alloc] initWithFrame:CGRectMake(20, y, contentWidth - 40, 20)];
+    info.text = @"🎮 资源仅供学习使用";
+    info.font = [UIFont systemFontOfSize:14];
+    info.textColor = [UIColor grayColor];
+    info.textAlignment = NSTextAlignmentCenter;
+    [self.contentView addSubview:info];
+    y += 30;
+    
+    // 提示
+    UILabel *tip = [[UILabel alloc] initWithFrame:CGRectMake(20, y, contentWidth - 40, 40)];
+    tip.text = @"注意：无敌功能暂未捕获到参数\n仅互秒功能可用";
+    tip.font = [UIFont systemFontOfSize:12];
+    tip.textColor = [UIColor redColor];
+    tip.textAlignment = NSTextAlignmentCenter;
+    tip.numberOfLines = 2;
+    [self.contentView addSubview:tip];
+    y += 48;
+    
+    // 按钮
+    UIButton *btn1 = [self createButtonWithTitle:@"⚔️ 互秒" tag:1];
+    btn1.frame = CGRectMake(20, y, contentWidth - 40, 35);
+    [self.contentView addSubview:btn1];
+    y += 43;
+    
+    UIButton *btn2 = [self createButtonWithTitle:@"🛡️ 无敌（未实现）" tag:2];
+    btn2.frame = CGRectMake(20, y, contentWidth - 40, 35);
+    btn2.enabled = NO;
+    btn2.alpha = 0.5;
+    [self.contentView addSubview:btn2];
+    y += 48;
+    
+    // 版权
+    UILabel *copyright = [[UILabel alloc] initWithFrame:CGRectMake(20, y, contentWidth - 40, 20)];
+    copyright.text = @"© 2026 IOSDK 科技虎";
+    copyright.font = [UIFont systemFontOfSize:12];
+    copyright.textColor = [UIColor lightGrayColor];
+    copyright.textAlignment = NSTextAlignmentCenter;
+    [self.contentView addSubview:copyright];
+}
+
+- (void)closeMenu {
+    [self removeFromSuperview];
+    g_menuView = nil;
+}
+
+- (UIButton *)createButtonWithTitle:(NSString *)title tag:(NSInteger)tag {
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn setTitle:title forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    btn.titleLabel.font = [UIFont systemFontOfSize:13 weight:UIFontWeightMedium];
+    btn.backgroundColor = [UIColor colorWithRed:0.8 green:0.2 blue:0.4 alpha:1];
+    btn.layer.cornerRadius = 12;
+    btn.tag = tag;
+    [btn addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    return btn;
+}
+
+- (void)buttonTapped:(UIButton *)sender {
+    switch (sender.tag) {
+        case 1: // 互秒
+            setGameValue(@"hook_int", @999999999, @"Number");
+            [self showAlert:@"⚔️ 互秒已开启！"];
+            break;
+        case 2: // 无敌（未实现）
+            [self showAlert:@"🛡️ 无敌功能暂未捕获到参数\n请等待后续更新"];
+            break;
+    }
+}
+
+- (void)showAlert:(NSString *)message {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示" message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil]];
+    UIViewController *rootVC = [UIApplication sharedApplication].keyWindow.rootViewController;
+    while (rootVC.presentedViewController) rootVC = rootVC.presentedViewController;
+    [rootVC presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint loc = [touch locationInView:self];
+    if (![self.contentView pointInside:[self.contentView convertPoint:loc fromView:self] withEvent:event]) {
+        [self removeFromSuperview];
+        g_menuView = nil;
+    }
+}
+@end
+
+#pragma mark - 悬浮按钮
+
+static UIWindow* getKeyWindow(void) {
+    UIWindow *keyWindow = nil;
+    for (UIWindow *window in [UIApplication sharedApplication].windows) {
+        if (window.isKeyWindow) {
+            keyWindow = window;
+            break;
+        }
+    }
+    if (!keyWindow) {
+        keyWindow = [UIApplication sharedApplication].windows.firstObject;
+    }
+    return keyWindow;
+}
+
+static void showMenu(void) {
+    if (g_menuView) {
+        [g_menuView removeFromSuperview];
+        g_menuView = nil;
+        return;
+    }
+    
+    UIWindow *keyWindow = getKeyWindow();
+    if (!keyWindow) return;
+    
+    CGRect windowBounds = keyWindow.bounds;
+    g_menuView = [[SGMenuView alloc] initWithFrame:windowBounds];
+    g_menuView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [keyWindow addSubview:g_menuView];
+}
+
+static void handlePan(UIPanGestureRecognizer *pan) {
+    UIWindow *keyWindow = getKeyWindow();
+    if (!keyWindow || !g_floatButton) return;
+    
+    CGPoint translation = [pan translationInView:keyWindow];
+    CGRect frame = g_floatButton.frame;
+    frame.origin.x += translation.x;
+    frame.origin.y += translation.y;
+    
+    CGFloat sw = keyWindow.bounds.size.width;
+    CGFloat sh = keyWindow.bounds.size.height;
+    frame.origin.x = MAX(0, MIN(frame.origin.x, sw - 50));
+    frame.origin.y = MAX(50, MIN(frame.origin.y, sh - 100));
+    
+    g_floatButton.frame = frame;
+    [pan setTranslation:CGPointZero inView:keyWindow];
+}
+
+static void setupFloatingButton(void) {
+    if (g_floatButton) return;
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIWindow *keyWindow = getKeyWindow();
+        if (!keyWindow) return;
+        
+        g_floatButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        g_floatButton.frame = CGRectMake(20, 100, 50, 50);
+        g_floatButton.backgroundColor = [UIColor colorWithRed:0.8 green:0.2 blue:0.4 alpha:0.9];
+        g_floatButton.layer.cornerRadius = 25;
+        g_floatButton.clipsToBounds = YES;
+        g_floatButton.layer.zPosition = 9999;
+        
+        [g_floatButton setTitle:@"💀" forState:UIControlStateNormal];
+        g_floatButton.titleLabel.font = [UIFont systemFontOfSize:28];
+        
+        [g_floatButton addTarget:[NSValue class] action:@selector(sg_showMenu) forControlEvents:UIControlEventTouchUpInside];
+        
+        UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:[NSValue class] action:@selector(sg_handlePan:)];
+        [g_floatButton addGestureRecognizer:pan];
+        
+        [keyWindow addSubview:g_floatButton];
+    });
+}
+
+@implementation NSValue (SGCheat)
++ (void)sg_showMenu { showMenu(); }
++ (void)sg_handlePan:(UIPanGestureRecognizer *)pan { handlePan(pan); }
+@end
+
+__attribute__((constructor))
+static void SGCheatInit(void) {
+    @autoreleasepool {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            setupFloatingButton();
+        });
+    }
+}
